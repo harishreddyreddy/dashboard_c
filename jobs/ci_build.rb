48fcb3f988b8351f5a5bdf7cc692ac40cca993b4
Harish Reddy <harishreddyreddy@gmail.com>
2015-03-19 22:54:24 -0700

    Add auth token env var and change data bind names

--- jobs/ci_build.rb
+++ jobs/ci_build.rb
@@ -3,23 +3,23 @@ SCHEDULER.every '20s', :first_in => 0  do
   tray = CCTray.new(ENV['CI_USER_NAME'], ENV['CI_PASSWORD'])
   projects = tray.fetch
 
-branche_names = {'coupa_development (master)' => 'Master',
-            'coupa_development (012_release)' => 'R12',
-            'coupa_development (012_release_unit_tests)' => 'R12 Unit',
-            'coupa_development (011_release)' => 'R11',
-            'coupa_development (012_0_4_release)' => 'R12_0_4',
-            'coupa_development (012_0_5_release)' => 'R12_0_5',
-            'coupa_development (011_0_17_release)' => 'R11_0_17'}
+branche_names = {'coupa_development (master)' => 'master',
+            'coupa_development (012_release)' => 'r12',
+            'coupa_development (012_release_unit_tests)' => 'r12_unit',
+            'coupa_development (011_release)' => 'r11',
+            'coupa_development (012_0_4_release)' => '12_0_4',
+            'coupa_development (012_0_5_release)' => '12_0_5',
+            'coupa_development (011_0_17_release)' => '11_0_17'}
 
-  result_override = {'Master' => false, 'R12'=> false, 'R12 Unit'=> false,
-                     'R11' => false, 'R12_0_4' => false, 'R12_0_5' => false, 'R11_0_17' => false}
+  result_override = {'master' => false, 'r12'=> false, 'r12_unit'=> false,
+                     'r11' => false, '12_0_4' => false, '12_0_5' => false, '11_0_17' => false}
 
   projects.each do |project|
       send_event(branche_names[project.name], {
         currentResult: result_override[branche_names[project.name]] ?  'Override' : project.last_build_status,
         lastResult: result_override[branche_names[project.name]] ?  'Override' : project.last_build_status ,
         timestamp: project.last_build_time_str,
-        message: branche_names[project.name] == 'R12_0_4' ? 'Unit & integration tests' : '' ,
+        message: branche_names[project.name] =~ /12_0_/ ? 'Unit & integration tests' : '' ,
         value: 0
       })
   end
