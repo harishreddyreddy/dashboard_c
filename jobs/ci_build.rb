ba75a9a0c8155640e9f0e32ddbc22c909e1187ba
Harish <harishreddyreddy@gmail.com>
2015-04-08 12:47:50 -0700

    Update ci_build.rb

--- jobs/ci_build.rb
+++ jobs/ci_build.rb
@@ -11,7 +11,7 @@ branche_names = {'coupa_development (master)' => 'master',
             'coupa_development (012_0_5_release)' => '12_0_5',
             'coupa_development (011_0_18_release)' => '11_0_18'}
 
-  result_override = {'master' => false, 'r12'=> false, 'r12_unit'=> true,
+  result_override = {'master' => false, 'r12'=> false, 'r12_unit'=> false,
                      'r11' => false, '12_0_4' => false, '12_0_5' => false, '11_0_18' => false}
 
   projects.each do |project|
