42d897af42cf7d33e548ccbfc2a8b66f496243db
Sangeetha Venkatraman <vsangeetha@users.noreply.github.com>
2015-07-14 15:10:53 -0700

    Update ci_build.rb

--- jobs/ci_build.rb
+++ jobs/ci_build.rb
@@ -9,11 +9,11 @@ branche_names = {'coupa_development (master)' => 'master',
             'coupa_development (012_release)' => 'r12',
             'coupa_development (012_release_unit_tests)' => 'r12_unit',
             'coupa_development (011_release)' => 'r11',
-            'coupa_development (012_0_11_release)' => '12_0_11',
+            'coupa_development (012_0_12_release)' => '12_0_12',
             'coupa_development (011_0_22_release)' => '11_0_22'}
 
   result_override = {'master' => false, '13_0_0'=> false, 'r12'=> false, 'r12_unit'=> false,
-                     'r11' => false, '12_0_11' => false, '11_0_22' => false}
+                     'r11' => false, '12_0_12' => false, '11_0_22' => false}
 
   projects.each do |project|
       send_event(branche_names[project.name], {
