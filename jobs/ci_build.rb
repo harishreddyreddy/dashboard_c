e5b9513277d2c0d2ea5c7427b4fb9552327592bb
Harish Reddy <harishreddyreddy@gmail.com>
2015-07-14 16:18:30 -0700

    Add 12_0_12

--- jobs/ci_build.rb
+++ jobs/ci_build.rb
@@ -8,7 +8,7 @@ branche_names = {'coupa_development (master)' => 'master',
             'coupa_development (012_release)' => 'r12',
             'coupa_development (012_release_unit_tests)' => 'r12_unit',
             'coupa_development (011_release)' => 'r11',
-            'coupa_development (012_0_11_release)' => '12_0_11',
+            'coupa_development (012_0_12_release)' => '12_0_12',
             'coupa_development (011_0_22_release)' => '11_0_22'}
 
   result_override = {'master' => false, 'r12'=> false, 'r12_unit'=> false,
